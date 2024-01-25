9240a5c14605c3e70f4345d483e152194d4656f6
Rowan Kinney <rk131321@ohio.edu>
2024-01-25 15:54:09 -0500

    blah

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Rowan Kinney>
+ *        Date: <01/25/24>
+ * Description: step 15
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
